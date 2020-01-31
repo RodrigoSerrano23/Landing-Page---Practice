e42c465ab1214215dbc0ccaf861407576b4896a5
Gabriela Orozco <gsoto@unitedvirtualities.com>
2020-01-31 13:28:53 -0700

    animations and modal window

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
+var topicLabel = document.getElementById('topicLabel');
+var topicText = document.getElementById('topicText');
+var topicImage = document.getElementById('topicImage');
+
 jQuery('document').ready(function($){
 
     var menuBtn= $('.menu-icon'),
@@ -13,3 +17,49 @@ jQuery('document').ready(function($){
             
         });
 });
+
+function showTopic(e){
+	switch(e){
+		case 1:
+			console.log("case 1");
+			getTopicData(e);// Populate card data
+			$('#topicModal').modal()// Open modal
+			break;
+		case 2:
+			console.log("case 2");
+			getTopicData(e);
+			$('#topicModal').modal();
+			break;
+		case 3:
+			console.log("case 3");
+			getTopicData(e);
+			$('#topicModal').modal();
+			break;
+		case 4:
+			console.log("case 4");
+			getTopicData(e);
+			$('#topicModal').modal();
+			break;
+		case 5:
+			console.log("case 5");
+			getTopicData(e);
+			$('#topicModal').modal();
+			break;
+		case 6:
+			console.log("case 6");
+			getTopicData(e);
+			$('#topicModal').modal();
+			break;
+		default:
+			console.log("invalid");
+			alert("Opción invalida")
+	}
+}
+
+// Get Data From content.js
+function getTopicData(e){
+	//Update labels with current data
+	topicLabel.innerHTML = topics["topic" + e].topicLabel
+	topicText.innerHTML = topics["topic" + e].topicText;
+	topicImage.src = topics["topic" + e].topicImage;
+}
